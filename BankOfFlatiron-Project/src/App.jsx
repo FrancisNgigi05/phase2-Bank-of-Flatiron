73b6ff09499e57ca41b86b1467c6f1c86a4b9f00
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2024-02-08 21:18:28 +0300

    On progress

--- BankOfFlatiron-Project/src/App.jsx
+++ BankOfFlatiron-Project/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [transactions, setTransactions] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		fetch("http://localhost:3000/transactions")
+			// Converting the data from the server to json format
+			.then((response) => response.json())
+			.then ((data) => setTransactions(data))
+	},[])// No Dependancy since I just want the data to be displayed onece and for all
+
+	// Using map to iterate over the data obtained to display it one by one
+	const transactionList = transactions.map((transaction) => {
+		return (
+			<li> {transaction.date}: {transaction.description}: {transaction.category}:  ${transaction.amount}</li>
+		)
+	})
+
+	return (
+		<ul>
+			{ transactionList }
+		</ul>
+	)
 }
 
 export default App
---
+++ BankOfFlatiron-Project/src/Form.js
@@ -0,0 +1,1 @@
+// This is where the coding of the form will be
