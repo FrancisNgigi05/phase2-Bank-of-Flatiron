4aad051b1a13ac047d32ed65aef59187bb8fd30c
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2024-02-16 15:03:35 +0300

    project

--- BankOfFlatiron-Project/src/App.jsx
+++ BankOfFlatiron-Project/src/App.jsx
@@ -1,27 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import TransactionTable from './components/TransactionTable';
+import TransactionForm from './components/TransactionForm';
+import SearchBar from './components/SearchBar';
+import './App.css';
+
+
 
 function App() {
-	const [transactions, setTransactions] = useState([])
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-	useEffect(() => {
-		fetch("http://localhost:3000/transactions")
-			// Converting the data from the server to json format
-			.then((response) => response.json())
-			.then ((data) => setTransactions(data))
-	},[])// No Dependancy since I just want the data to be displayed onece and for all
+  useEffect(() => {
+    fetch('http://localhost:4000/transactions')
+      .then((response) => response.json())
+      .then((data) => setTransactions(data));
+  }, []);
 
-	// Using map to iterate over the data obtained to display it one by one
-	const transactionList = transactions.map((transaction) => {
-		return (
-			<li> {transaction.date}: {transaction.description}: {transaction.category}:  ${transaction.amount}</li>
-		)
-	})
+  const addTransaction = (newTransaction) => {
+   
+    newTransaction.id = Math.random(); 
 
-	return (
-		<ul>
-			{ transactionList }
-		</ul>
-	)
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const deleteTransaction = (id) => {
+    const updatedTransactions = transactions.filter(
+      (transaction) => transaction.id !== id
+    );
+    setTransactions(updatedTransactions);
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.includes(searchTerm)
+  );
+
+  return (
+    <div>
+      <h1>Bank Transactions</h1>
+
+      
+      <TransactionForm addTransaction={addTransaction} />
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <TransactionTable
+        transactions={filteredTransactions}
+        deleteTransaction={deleteTransaction}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
